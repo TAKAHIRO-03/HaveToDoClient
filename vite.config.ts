de1b27cdf1242e812ac0b88b84b286ff5082fe06
TAKAHIRO-03 <1701010402qb@cyber-u.ac.jp>
2022-05-18 13:57:01 +0000

    ディレクトリの整理、ルーティングの設定

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './src',
   plugins: [react()],
+  build: {
+    outDir: '../public',
+  },
   server: {
     host: '0.0.0.0',
     port: 8888
