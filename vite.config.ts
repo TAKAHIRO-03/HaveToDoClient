f201a2553ee1815ec918109bc4a28208364c2ea5
TAKAHIRO-03 <1701010402qb@cyber-u.ac.jp>
2022-06-05 16:34:39 +0900

    デプロイ時の設定修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   root: './src',
   plugins: [react()],
   build: {
     outDir: '../public',
+    emptyOutDir: true,
   },
   server: {
     host: '0.0.0.0',
