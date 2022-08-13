99853e5ac66bb072307d680b28b7a755c2389df4
TAKAHIRO-03 <1701010402qb@cyber-u.ac.jp>
2022-08-13 16:41:36 +0900

    mui導入

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  root: './src',
-  plugins: [react()],
+  base: "./",
+  root: "./src",
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+    }),
+  ],
   build: {
-    outDir: '../public',
+    outDir: "../public",
     emptyOutDir: true,
   },
   server: {
-    host: '0.0.0.0',
-    port: 8888
+    host: "0.0.0.0",
+    port: 8888,
   },
-})
+});
