ef58f7ff4bab77970bb0b414c41832ee0f10292f
TAKAHIRO-03 <1701010402qb@cyber-u.ac.jp>
2022-08-13 16:51:28 +0900

    ログイン画面追加

---
+++ src/api/rest/LoginRepository.tsx
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { BaseRepository } from "./base/BaseRepository";
+import { BaseRequest } from "./base/BaseRequest";
+import {
+  BaseErrorResponse,
+  BaseResponse,
+  CommonErrorResponse,
+  CommonResponse,
+} from "./base/BaseResponse";
+
+export class LoginRequest extends BaseRequest {
+  public readonly username: string;
+  public readonly password: string;
+
+  constructor(username: string, password: string) {
+    super();
+    this.username = username;
+    this.password = password;
+  }
+}
+
+export class LoginRepository extends BaseRepository {
+  public async post(
+    req: LoginRequest
+  ): Promise<BaseResponse | BaseErrorResponse> {
+    console.log(JSON.stringify(req));
+
+    const res = await axios
+      .post(this.baseUrl! + "/api/v1.0/login", JSON.stringify(req), {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        return response;
+      })
+      .catch((error) => {
+        return error;
+      });
+
+    if (axios.isAxiosError(res)) {
+      return new CommonErrorResponse(res);
+    }
+
+    return new CommonResponse(res);
+  }
+  public get(...args: any): CommonResponse {
+    throw new Error("Method not implemented.");
+  }
+  public patch(args: unknown): CommonResponse {
+    throw new Error("Method not implemented.");
+  }
+  public delete(...args: any): CommonResponse {
+    throw new Error("Method not implemented.");
+  }
+}
---
+++ src/components/pages/Error.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Error = () => {
+  return (
+    <main>
+      <h2>Error!!</h2>
+      <p>テストテキストテストテキストテストテキスト</p>
+      <Link to="/">戻る</Link>
+    </main>
+  );
+};
+
+export default Error;
